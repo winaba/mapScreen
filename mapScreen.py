b7cf721e2d89095e271cfd24074e3941004a5430
Wagner Inaba <wagner.inaba@gmail.com>
2020-02-01 13:57:57 -0300

    Separa o código em funções, para facilitar a leitura

--- mapScreen.py
+++ mapScreen.py
@@ -1,73 +1,81 @@
 import pyautogui     
 from arquivoDeConfiguracao import ArquivoDeConfiguracao
 
-print("")
-print("##################################")
-print("Vamos configurar os documentos SAT")
-print("##################################")
-print("")
-print('Coloque o mouse sobre o local que ficará os campos mencionados e precione [Enter]')
-print("")
-print('Coloque o mouse sobre')
-
 config = ArquivoDeConfiguracao()
 
-input('- O campo chave de acesso: ')
+def configuraArquivoSAT():
+    print("")
+    print("##################################")
+    print("Vamos configurar os documentos SAT")
+    print("##################################")
+    print("")
+    print('Coloque o mouse sobre o local que ficará os campos mencionados e precione [Enter]')
+    print("")
+    print('Coloque o mouse sobre')
 
-config.addLine('CHAVE_ACESSO_X', str(pyautogui.position()[0]) )
-config.addLine('CHAVE_ACESSO_Y', str(pyautogui.position()[1]) )
+    input('- O campo chave de acesso: ')
 
-print('Leitura realizada')
+    config.addLine('CHAVE_ACESSO_X', str(pyautogui.position()[0]) )
+    config.addLine('CHAVE_ACESSO_Y', str(pyautogui.position()[1]) )
 
-input("- O botão [Salvar Nota]: ")
+    print('Leitura realizada')
 
-config.addLine('SALVAR_NOTA_SAT_X', str(pyautogui.position()[0]) )
-config.addLine('SALVAR_NOTA_SAT_Y', str(pyautogui.position()[1]) )
+    input("- O botão [Salvar Nota]: ")
 
-print('Leitura realizada')
+    config.addLine('SALVAR_NOTA_SAT_X', str(pyautogui.position()[0]) )
+    config.addLine('SALVAR_NOTA_SAT_Y', str(pyautogui.position()[1]) )
 
-print("")
-print("####################################")
-print("Vamos configurar os documentos Cupom")
-print("####################################")
-print("")
-print('Coloque o mouse sobre o local que ficará os campos mencionados e precione [Enter]')
-print("")
-print('Coloque o mouse sobre')
+    print('Leitura realizada')
 
-input('- O campo CNPJ do Emissor da Nota: ')
+def configuraArquivoCupom():    
+    print("")
+    print("####################################")
+    print("Vamos configurar os documentos Cupom")
+    print("####################################")
+    print("")
+    print('Coloque o mouse sobre o local que ficará os campos mencionados e precione [Enter]')
+    print("")
+    print('Coloque o mouse sobre')
 
-config.addLine('CNPJ_EMISSOR_X', str(pyautogui.position()[0]) )
-config.addLine('CNPJ_EMISSOR_Y', str(pyautogui.position()[1]) )
+    input('- O campo CNPJ do Emissor da Nota: ')
 
-print('Leitura realizada')
+    config.addLine('CNPJ_EMISSOR_X', str(pyautogui.position()[0]) )
+    config.addLine('CNPJ_EMISSOR_Y', str(pyautogui.position()[1]) )
 
-input("- O botão [Salvar Nota]: ")
+    print('Leitura realizada')
 
-config.addLine('SALVAR_NOTA_CUPOM_X', str(pyautogui.position()[0]) )
-config.addLine('SALVAR_NOTA_CUPOM_Y', str(pyautogui.position()[1]) )
+    input("- O botão [Salvar Nota]: ")
 
-print('Leitura realizada')
+    config.addLine('SALVAR_NOTA_CUPOM_X', str(pyautogui.position()[0]) )
+    config.addLine('SALVAR_NOTA_CUPOM_Y', str(pyautogui.position()[1]) )
 
-print("")
-print("#######################################")
-print("Agora vamos configurar outros controles")
-print("#######################################")
-print("")
+    print('Leitura realizada')
 
-config.addLine('ERROS_SEGUIDOS', input("- Qual a quantidade de erros seguidos que podem ocorrer? "))
+def configuraControles():
+    print("")
+    print("#######################################")
+    print("Agora vamos configurar outros controles")
+    print("#######################################")
+    print("")
 
-config.addLine('SAT_TOTAL_MINUTO', input("- Qual a quantidade de erros seguidos que podem ocorrer? "))
+    config.addLine('ERROS_SEGUIDOS', input("- Qual a quantidade de erros seguidos que podem ocorrer? "))
 
-config.addLine('CUPOM_TOTAL_MINUTO', input("- Qual a quantidade de erros seguidos que podem ocorrer? "))
+    config.addLine('SAT_TOTAL_MINUTO', input("- Qual a quantidade de erros seguidos que podem ocorrer? "))
 
+    config.addLine('CUPOM_TOTAL_MINUTO', input("- Qual a quantidade de erros seguidos que podem ocorrer? "))
 
-print("")
-print("##############################")
-print("Visualização do arquivo gerado")
-print("##############################")
-print("")
+def visualizaArquivoDeConfiguracao():
+    print("")
+    print("##############################")
+    print("Visualização do arquivo gerado")
+    print("##############################")
+    print("")
 
-config.content()
+    config.content()
 
-config.post()
+if __name__ == "__main__":
+    configuraArquivoSAT()
+    configuraArquivoCupom()
+    configuraControles()
+    visualizaArquivoDeConfiguracao()
+    config.post()
