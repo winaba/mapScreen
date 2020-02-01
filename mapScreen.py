673de22bb1ce24a5249dd5f3d167d6da91311c33
Wagner Inaba <wagner.inaba@gmail.com>
2020-02-01 14:20:07 -0300

    Utilização da classe RotinasDeConfiguracao

--- mapScreen.py
+++ mapScreen.py
@@ -1,81 +1,11 @@
-import pyautogui     
-from arquivoDeConfiguracao import ArquivoDeConfiguracao
-
-config = ArquivoDeConfiguracao()
-
-def configuraArquivoSAT():
-    print("")
-    print("##################################")
-    print("Vamos configurar os documentos SAT")
-    print("##################################")
-    print("")
-    print('Coloque o mouse sobre o local que ficará os campos mencionados e precione [Enter]')
-    print("")
-    print('Coloque o mouse sobre')
-
-    input('- O campo chave de acesso: ')
-
-    config.addLine('CHAVE_ACESSO_X', str(pyautogui.position()[0]) )
-    config.addLine('CHAVE_ACESSO_Y', str(pyautogui.position()[1]) )
-
-    print('Leitura realizada')
-
-    input("- O botão [Salvar Nota]: ")
-
-    config.addLine('SALVAR_NOTA_SAT_X', str(pyautogui.position()[0]) )
-    config.addLine('SALVAR_NOTA_SAT_Y', str(pyautogui.position()[1]) )
-
-    print('Leitura realizada')
-
-def configuraArquivoCupom():    
-    print("")
-    print("####################################")
-    print("Vamos configurar os documentos Cupom")
-    print("####################################")
-    print("")
-    print('Coloque o mouse sobre o local que ficará os campos mencionados e precione [Enter]')
-    print("")
-    print('Coloque o mouse sobre')
-
-    input('- O campo CNPJ do Emissor da Nota: ')
-
-    config.addLine('CNPJ_EMISSOR_X', str(pyautogui.position()[0]) )
-    config.addLine('CNPJ_EMISSOR_Y', str(pyautogui.position()[1]) )
-
-    print('Leitura realizada')
-
-    input("- O botão [Salvar Nota]: ")
-
-    config.addLine('SALVAR_NOTA_CUPOM_X', str(pyautogui.position()[0]) )
-    config.addLine('SALVAR_NOTA_CUPOM_Y', str(pyautogui.position()[1]) )
-
-    print('Leitura realizada')
-
-def configuraControles():
-    print("")
-    print("#######################################")
-    print("Agora vamos configurar outros controles")
-    print("#######################################")
-    print("")
-
-    config.addLine('ERROS_SEGUIDOS', input("- Qual a quantidade de erros seguidos que podem ocorrer? "))
-
-    config.addLine('SAT_TOTAL_MINUTO', input("- Qual a quantidade de erros seguidos que podem ocorrer? "))
-
-    config.addLine('CUPOM_TOTAL_MINUTO', input("- Qual a quantidade de erros seguidos que podem ocorrer? "))
-
-def visualizaArquivoDeConfiguracao():
-    print("")
-    print("##############################")
-    print("Visualização do arquivo gerado")
-    print("##############################")
-    print("")
-
-    config.content()
+from rotinasDeConfiguracao import RotinasDeConfiguracao
 
 if __name__ == "__main__":
-    configuraArquivoSAT()
-    configuraArquivoCupom()
-    configuraControles()
-    visualizaArquivoDeConfiguracao()
-    config.post()
+    
+    a = RotinasDeConfiguracao()
+
+    a.configuraArquivoSAT()
+    a.configuraArquivoCupom()
+    a.configuraControles()
+    a.visualizaArquivoDeConfiguracao()
+    a.gravaArquivo()
